7b9daa23b0acdaea7972baf85f68ed53c0144b48
SamH44 <samuel.harkness44@gmail.com>
2025-10-15 17:14:28 -0400

    Imported crossbar, attached script to object, verified that joint modification works, working on controlling crossbar with inputs

---
+++ Assets/Scripts/MarionetteControl.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MarionetteControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameObject centerJoint = GameObject.Find("crossbar/Joints/center");
+        GameObject leftArmJoint = GameObject.Find("crossbar/Joints/arm_L");
+        GameObject rightArmJoint = GameObject.Find("crossbar/Joints/arm_R");
+        GameObject leftLegJoint = GameObject.Find("crossbar/Joints/leg_L");
+        GameObject rightLegJoint = GameObject.Find("crossbar/Joints/leg_R");
+        if (centerJoint != null)
+        {
+            centerJoint.transform.localRotation = Quaternion.Euler(60, 0, 0);
+        }
+        else
+        {
+            Debug.LogError("Center joint not found!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
