db63462080de1511aa4afce856708971b16e4070
SamH44 <samuel.harkness44@gmail.com>
2025-10-17 16:54:27 -0400

    Imported a 3D body model, added ragdoll physics, implemented logic for calculating "tension" between crossbar and puppet joints,

---
+++ Assets/Editor/CopyPathFromScene.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class CopyGameObjectPathFromHierarchy
+{
+    [MenuItem("GameObject/Copy Path", false, 11)]
+    static void CopyPath()
+    {
+        GameObject currentGameObject = Selection.activeGameObject;
+        if (currentGameObject == null)
+            return;
+
+        string path = currentGameObject.name;
+        while (currentGameObject.transform.parent != null)
+        {
+            currentGameObject = currentGameObject.transform.parent.gameObject;
+            path = $"{currentGameObject.name}/{path}";
+        }
+        EditorGUIUtility.systemCopyBuffer = path;
+        Debug.Log($"Copied GameObject path: {path}");
+    }
+
+    /// <summary>
+    /// Only allow path copying if 1 object is selected.
+    /// </summary>
+    [MenuItem("GameObject/Copy Path", true)]
+    static bool CopyPathValidation() => Selection.gameObjects.Length == 1;
+}
--- Assets/Scripts/MarionetteControl.cs
+++ Assets/Scripts/MarionetteControl.cs
@@ -20,11 +20,30 @@ public class MarionetteControl : MonoBehaviour
 
     public float rotationSpeed;
 
-    GameObject centerJoint;
-    GameObject leftArmJoint; 
-    GameObject rightArmJoint;
-    GameObject leftLegJoint;
-    GameObject rightLegJoint;
+    public Vector3 startingPosition;
+
+    public float headStringLength;
+    public float armsStringLength;
+    public float legsStringLength;
+    public float tensionMultiplier;
+
+    GameObject crossbarCenterJoint;
+    GameObject crossbarLeftArmJoint; 
+    GameObject crossbarRightArmJoint;
+    GameObject crossbarLeftLegJoint;
+    GameObject crossbarRightLegJoint;
+
+    GameObject modelCenterJoint;
+    GameObject modelLeftArmJoint;
+    GameObject modelRightArmJoint;
+    GameObject modelLeftLegJoint;
+    GameObject modelRightLegJoint;
+
+    Rigidbody centerRigidbody;
+    Rigidbody leftArmRigidbody;
+    Rigidbody rightArmRigidbody;
+    Rigidbody leftLegRigidbody;
+    Rigidbody rightLegRigidbody;
 
     Vector2 movementInput;
 
@@ -41,19 +60,32 @@ public class MarionetteControl : MonoBehaviour
     void Start()
     {
 
-        centerJoint = GameObject.Find("crossbar/Joints/center");
-        leftArmJoint = GameObject.Find("crossbar/Joints/arm_L");
-        rightArmJoint = GameObject.Find("crossbar/Joints/arm_R");
-        leftLegJoint = GameObject.Find("crossbar/Joints/leg_L");
-        rightLegJoint = GameObject.Find("crossbar/Joints/leg_R");
-        if (centerJoint != null)
-        {
-            centerJoint.transform.localRotation = Quaternion.Euler(-60, 0, 0);
-        }
-        else
-        {
-            Debug.LogError("Center joint not found!");
-        }
+        crossbarCenterJoint = GameObject.Find("crossbar/Joints/center");
+        crossbarLeftArmJoint = GameObject.Find("crossbar/Joints/center/arm_L");
+        crossbarRightArmJoint = GameObject.Find("crossbar/Joints/center/arm_R");
+        crossbarLeftLegJoint = GameObject.Find("crossbar/Joints/center/leg_L");
+        crossbarRightLegJoint = GameObject.Find("crossbar/Joints/center/leg_R");
+
+        xPos = startingPosition.x;
+        yPos = startingPosition.y;
+
+
+        //get all joints from the model where the "strings" will be attached
+        //assuming a specific hierarchy here - will need to be adjusted based on model naming conventions
+        modelCenterJoint = GameObject.Find("model/DEFORM_RIG/root/DEF_hipJA_1/DEF_spineJA_1/DEF_spineJB_1/DEF_spineJC_1/DEF_chestJA_1/DEF_neckJA_1");
+        modelLeftArmJoint = GameObject.Find("model/DEFORM_RIG/root/DEF_hipJA_1/DEF_spineJA_1/DEF_spineJB_1/DEF_spineJC_1/DEF_chestJA_1/DEF_armJALt_1/DEF_shoulderJALt_1/DEF_elbowJALt_1/DEF_wristJALt_1");
+        modelRightArmJoint = GameObject.Find("model/DEFORM_RIG/root/DEF_hipJA_1/DEF_spineJA_1/DEF_spineJB_1/DEF_spineJC_1/DEF_chestJA_1/DEF_armJARt_1/DEF_shoulderJARt_1/DEF_elbowJARt_1/DEF_wristJARt_1");
+        modelLeftLegJoint = GameObject.Find("model/DEFORM_RIG/root/DEF_hipJA_1/DEF_legJARt_1/DEF_kneeJARt_1");
+        modelRightLegJoint = GameObject.Find("model/DEFORM_RIG/root/DEF_hipJA_1/DEF_legJALt_1/DEF_kneeJALt_1");
+
+        //get rigidbodies for each joint: forces will be applied to these
+        centerRigidbody = modelCenterJoint.GetComponent<Rigidbody>();
+        leftArmRigidbody = modelLeftArmJoint.GetComponent<Rigidbody>();
+        rightArmRigidbody = modelRightArmJoint.GetComponent<Rigidbody>();
+        leftLegRigidbody = modelLeftLegJoint.GetComponent<Rigidbody>();
+        rightLegRigidbody = modelRightLegJoint.GetComponent<Rigidbody>();
+
+
     }
 
     void OnMove(InputValue value)
@@ -75,9 +107,31 @@ public class MarionetteControl : MonoBehaviour
 
         Vector3 targetRotation = new Vector3(-15 - targetYRot, 0, -1 * targetXRot);
 
-        centerJoint.transform.localPosition = new Vector3(xPos, yPos, 0);
-        centerJoint.transform.localRotation = Quaternion.RotateTowards(centerJoint.transform.localRotation, Quaternion.Euler(targetRotation), rotationSpeed *  Time.deltaTime);
+        crossbarCenterJoint.transform.localPosition = new Vector3(xPos, yPos, 0);
+        crossbarCenterJoint.transform.localRotation = Quaternion.RotateTowards(crossbarCenterJoint.transform.localRotation, Quaternion.Euler(targetRotation), rotationSpeed *  Time.deltaTime);
+
+        //apply tension forces to each joint
+        centerRigidbody.AddForce(CalculateTension(crossbarCenterJoint.transform.position, modelCenterJoint.transform.position, headStringLength, tensionMultiplier * 2));
+        leftArmRigidbody.AddForce(CalculateTension(crossbarLeftArmJoint.transform.position, modelLeftArmJoint.transform.position, armsStringLength, tensionMultiplier));
+        rightArmRigidbody.AddForce(CalculateTension(crossbarRightArmJoint.transform.position, modelRightArmJoint.transform.position, armsStringLength, tensionMultiplier));
+        leftLegRigidbody.AddForce(CalculateTension(crossbarLeftLegJoint.transform.position, modelLeftLegJoint.transform.position, legsStringLength, tensionMultiplier));
+        rightLegRigidbody.AddForce(CalculateTension(crossbarRightLegJoint.transform.position, modelRightLegJoint.transform.position, legsStringLength, tensionMultiplier));
 
 
     }
+
+
+    Vector3 CalculateTension(Vector3 crossbarJointPosition, Vector3 modelJointPosition, float baseLength, float tensionMultiplier)
+    {
+        Vector3 modelToCrossbar = new Vector3();
+        modelToCrossbar = crossbarJointPosition - modelJointPosition;
+
+        float currentLength = modelToCrossbar.magnitude;
+        float stretchLength = currentLength - baseLength;
+        stretchLength = Mathf.Clamp(stretchLength, 0, 100);
+
+        return modelToCrossbar.normalized * stretchLength * tensionMultiplier;
+
+    }
+
 }
